389f57fb3c1e92ba6ca331f28dc1bcd4f5365355
Sakshi <sakshi.yadav@mobcoder.com>
2021-01-08 21:53:45 +0530

    changes

--- src/main/java/com/loando/userService/launcher/BffConsumerServiceApplication.java
+++ src/main/java/com/loando/userService/launcher/BffConsumerServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication()
 @ComponentScan("com.loando.**")
 @EnableDiscoveryClient
 public class BffConsumerServiceApplication {
